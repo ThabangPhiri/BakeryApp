6170e72f01c3a5fc094dfa356a31be86edd7cd69
Thabang <thabangphiri07@gmail.com>
2024-09-23 19:19:54 +0200

    first commit

--- my-react-app/my-react-app/src/state/Pages/App.jsx
+++ my-react-app/my-react-app/src/state/Pages/App.jsx
@@ -2,7 +2,6 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Cakes from './Cakes.jsx'
 import Card from './Card.jsx'
-import Button from './Button.jsx'
 import ItemCard from './ItemCard.js';
 import TestComponent from './state/TestComponent';
 
@@ -18,7 +17,6 @@ function App() {
     <>
     <Cakes/>
     <Card/>
-    <Button/>
     <ItemCard/>
     <Footer/>
     <Header/>
